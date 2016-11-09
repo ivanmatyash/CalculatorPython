82c7492e52711afaa346da3c4e48da38fc72cb87
ivan Matyash <ivan.matyash97@gmail.com>
2016-11-10 02:32:41 +0300

    bugs fixing

--- calc.py
+++ calc.py
@@ -13,6 +13,8 @@ def makeOperation(operation):
 			result_str.append(math.sin(right))
 		if operation == 'sqrt':
 			result_str.append(math.sqrt(right))
+		if operation == 'cos':
+			result_str.append(math.cos(right))
 	else:
 		right = result_str.pop()
 		left = result_str.pop()
@@ -39,13 +41,14 @@ def printStacks():
 	print("str= ", result_str)
 	print("ope= ", operations)
 
-input_str = '1*4+3.3/(3 + .3)*3*(sqrt(4))/(sin(0) + 1)'
+input_str = '1*4+3.3/(3 + .3)*3*(sqrt(4))/(cos(0) + 1)'
 
 operations = []
 result_str = []
 
 operand = ''
 for elem in input_str:
+
 	if elem == ' ':
 		continue
 
@@ -57,27 +60,29 @@ for elem in input_str:
 				result_str.append(float(operand))
 			operand = ''
 		operations.append(elem)
-		printStacks()
+		
 		continue
 	elif elem == ')':
 		if operand != '':
 			result_str.append(float(operand))
 			operand = ''
-			printStacks()		
+					
 		while operations[-1] != '(':
+			
 			makeOperation(operations.pop())
 		operations.pop()
-		printStacks()
+		
 		continue
 	elif isOperation(elem):
 		if operand != '':
-			print ("OPERAND =" + operand)
+		#	print ("OPERAND =" + operand)
 			result_str.append(float(operand))
 			operand = ''
 		while operations and priority(operations[-1]) >= priority(elem):
 			makeOperation(operations.pop())
+			
 		operations.append(elem)
-		printStacks()
+		
 	else:
 		operand += elem
 
@@ -85,11 +90,11 @@ for elem in input_str:
 if (operand != ''):
 	result_str.append(float(operand))
 
-printStacks()
+
 		 
 while operations:
 	makeOperation(operations.pop())	
-printStacks()
+
 		
 print('lol')
 print (result_str)
